09943539c09b038f53a4e74e3d9b98b761c2370a
Michał Czernek <czernekmichal@gmail.com>
2020-09-08 13:48:50 +0200

    Translate both drawer and content.

--- App.tsx
+++ App.tsx
@@ -9,41 +9,101 @@
 import React from 'react';
 import { View, StyleSheet, StatusBar, Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  useSharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 const App = () => {
+  const translateDrawer = useSharedValue(0 as number);
+  const translateContent = useSharedValue(0 as number);
+
+  const drawerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateDrawer.value,
+        },
+      ],
+    };
+  });
+
+  const contentStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateContent.value,
+        },
+      ],
+    };
+  });
+
+  const onPan = useAnimatedGestureHandler({
+    onStart: (event, context) => {},
+    onActive: (event, context) => {
+      translateDrawer.value = event.translationX;
+      translateContent.value = event.translationX;
+    },
+    onEnd: (event, context) => {},
+  });
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaProvider style={styles.constainer}>
-        <SafeAreaView style={styles.drawerColumnContainer}>
-          <View style={styles.drawerColumn}>
-            <Text>Drawer column</Text>
-          </View>
-        </SafeAreaView>
-        <SafeAreaView style={styles.entriesColumnContainer}>
-          <View style={styles.entriesColumn}>
-            <Text>Entries column</Text>
-          </View>
-        </SafeAreaView>
-        <SafeAreaView style={styles.contentColumnContainer}>
-          <View style={styles.contentColumn}>
-            <Text>Content column</Text>
-          </View>
-        </SafeAreaView>
+        <PanGestureHandler onGestureEvent={onPan}>
+          <Animated.View style={styles.contentContainer}>
+            <Animated.View style={[styles.movingContentConteiner, contentStyle]}>
+              <SafeAreaView style={styles.entriesColumnContainer}>
+                <View style={styles.entriesColumn}>
+                  <Text>Entries column</Text>
+                </View>
+              </SafeAreaView>
+              <SafeAreaView style={styles.contentColumnContainer}>
+                <View style={styles.contentColumn}>
+                  <Text>Content column</Text>
+                </View>
+              </SafeAreaView>
+            </Animated.View>
+            <Animated.View style={[styles.drawerColumnContainer, drawerStyle]}>
+              <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.drawerColumn}>
+                  <Text>Drawer column</Text>
+                </View>
+              </SafeAreaView>
+            </Animated.View>
+          </Animated.View>
+        </PanGestureHandler>
       </SafeAreaProvider>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  constainer: {
+  constainer: {},
+  contentContainer: {
     backgroundColor: '#ccaadd',
+    flex: 1,
+  },
+  movingContentConteiner: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    start: 0,
+    end: 0,
     flexDirection: 'row',
     alignItems: 'stretch',
     justifyContent: 'center',
   },
   drawerColumnContainer: {
-    flex: 2,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    start: -250,
+    backgroundColor: '#ffeecc',
+    width: 250,
   },
   drawerColumn: {
     backgroundColor: '#ffeecc',
